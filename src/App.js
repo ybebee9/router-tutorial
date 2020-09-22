400aa90bccd119da8d922fcd40717d720641ec92
ybebee9 <ybebee9@gmail.com>
2020-09-22 09:40:32 +0900

    router 부가기능 history, withRouter, Switch 실습

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import {Link, Route} from 'react-router-dom';
+import {Link, Route, Switch} from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import Profiles from "./Profiles";
+import HistorySample from "./HistorySample";
 
 function App() {
   return (
@@ -13,9 +15,27 @@ function App() {
             <li>
                 <Link to="/about">소개</Link>
             </li>
+            <li>
+                <Link to="/profiles">프로필</Link>
+            </li>
+            <li>
+                <Link to="/history">History 예제</Link>
+            </li>
         </ul>
-      <Route path="/" component={Home} exact={true} />
-      <Route path={['/about','/info']} component={About} />
+        <hr />
+        <Switch>
+            <Route path="/" component={Home} exact={true} />
+            <Route path={['/about','/info']} component={About} />
+            <Route path="/profiles" component={Profiles} />
+            <Route path="/history" component={HistorySample} />
+            <Route render={({ location }) => (
+                <div>
+                    <h2>이 페이지는 존재하지 않습니다:(</h2>
+                    <p>{location.pathname}</p>
+                </div>
+            )} />
+        </Switch>
+
     </div>
   );
 }
